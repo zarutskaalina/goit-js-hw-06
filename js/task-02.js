00ea33a352e99a9d7cce0dfcc292a6fa8154a9dd
Zarutska Alina <zaryckaalina@gmail.com>
2023-08-05 15:20:12 +0300

    the second task is done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+ingredients.forEach(ingredient => {
+  const listElement = document.createElement('li');
+  listElement.classList.add('item');
+  listElement.textContent = ingredient;
+  ingredientsList.append(listElement);
+  console.dir(listElement);
+})
